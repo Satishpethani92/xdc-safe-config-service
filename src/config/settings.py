bd357377b2f1ab60426cfe28add8d032543e2c3e
Satish pethani <satyapethani@gmail.com>
2025-06-26 17:04:37 +0530

    Minor Fix

--- src/config/settings.py
+++ src/config/settings.py
@@ -144,7 +144,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_NAME", "postgres"),
+        "NAME": os.getenv("POSTGRES_DB", "postgres"),
         "USER": os.getenv("POSTGRES_USER", "postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
         "HOST": os.getenv("POSTGRES_HOST", "localhost"),
